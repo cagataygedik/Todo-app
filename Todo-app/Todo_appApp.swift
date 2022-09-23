f1d8e117134e19c6f5cb703f5d8f508ec9f26e6a
cagatay <cagatay.gedik@icloud.com>
2022-09-23 12:26:48 +0300

    Initial Commit

---
+++ Todo-app/Todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_appApp.swift
+//  Todo-app
+//
+//  Created by Celil Çağatay Gedik on 23.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
