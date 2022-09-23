e8d23b7b49b4c2a1eb7bd7fe717051bfcbe9c1f8
cagatay <cagatay.gedik@icloud.com>
2022-09-23 15:12:34 +0300

    added ItemModels

---
+++ Todo-app/Models/ItemModel.swift
@@ -0,0 +1,26 @@
+//
+//  ItemModel.swift
+//  Todo-app
+//
+//  Created by Celil Çağatay Gedik on 23.09.2022.
+//
+
+import Foundation
+
+struct ItemModel: Identifiable {
+    let id: String
+    let title: String
+    let isCompleted: Bool
+    
+    init(id: String = UUID().uuidString, title: String, isCompleted: Bool) {
+        self.id = UUID().uuidString
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+    
+    func updateComplete() -> ItemModel {
+        return ItemModel(id: id, title: title, isCompleted: !isCompleted)
+    }
+}
+
+
