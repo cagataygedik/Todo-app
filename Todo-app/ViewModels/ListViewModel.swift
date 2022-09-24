eef4c8d036f65133481718d569507a5d730271c3
cagatay <cagatay.gedik@icloud.com>
2022-09-24 12:09:39 +0300

    improved CRUD functions

--- Todo-app/ViewModels/ListViewModel.swift
+++ Todo-app/ViewModels/ListViewModel.swift
@@ -7,25 +7,32 @@
 
 import Foundation
 /*
-
+ 
  CRUD FUNCTIONS
  
  */
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    
+    
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the first title", isCompleted: false),
-            ItemModel(title: "second", isCompleted: true),
-            ItemModel(title: "third", isCompleted: false)
-        ]
-        items.append(contentsOf: newItems)
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        
+        self.items = savedItems
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -50,6 +57,12 @@ class ListViewModel: ObservableObject {
         
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = item.updateComplete()
+        }
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
 }
